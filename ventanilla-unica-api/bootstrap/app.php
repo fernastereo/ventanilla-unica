c91edff5cfcb8ea92820c7ab84f51c6b0275e331
Fernando Cueto <fernandoecueto@gmail.com>
2024-08-13 15:54:32 +0200

    fix axios request

--- ventanilla-unica-api/bootstrap/app.php
+++ ventanilla-unica-api/bootstrap/app.php
@@ -20,9 +20,9 @@ return Application::configure(basePath: dirname(__DIR__))
             'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
         ]);
 
-        $middleware->validateCsrfTokens(except: [
-            '/api/peticions',
-        ]);
+        // $middleware->validateCsrfTokens(except: [
+        //     '/api/peticions',
+        // ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
