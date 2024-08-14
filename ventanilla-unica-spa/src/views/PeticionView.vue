06ef1466b6644753cce8dee7f855a1c75789a227
Fernando Cueto <fernandoecueto@gmail.com>
2024-08-14 17:56:33 +0200

    fix bug in peticion for axios request

--- ventanilla-unica-spa/src/views/PeticionView.vue
+++ ventanilla-unica-spa/src/views/PeticionView.vue
@@ -23,6 +23,8 @@
   });
 
   const handleSubmit = async (e) => {
+    await axios.get(apiUrl + '/sanctum/csrf-cookie');
+    
     const result = await axios.post(apiUrl + '/api/peticions', form, {});
     
     showResult.value = result.data.radicado;
