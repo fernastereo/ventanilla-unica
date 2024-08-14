5455faaac61e2841a2338d988815528ba592c950
Fernando Cueto <fernandoecueto@gmail.com>
2024-08-14 18:01:57 +0200

    fix routes on backend

--- ventanilla-unica-api/routes/api.php
+++ ventanilla-unica-api/routes/api.php
@@ -14,10 +14,10 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::get('/dashboard', function () {
         return response()->json(['message' => 'Dashboard accessed successfully!']);
     });
+    Route::apiResource('peticions', PeticionController::class);
+    Route::get('/tipopeticions', [TipopeticionController::class, 'index']);
 });
 
-Route::apiResource('peticions', PeticionController::class);
-Route::get('/tipopeticions', [TipopeticionController::class, 'index']);
 
 Route::get('/test', function () {
     return response()->json(['message' => 'Test performed successfully!']);
