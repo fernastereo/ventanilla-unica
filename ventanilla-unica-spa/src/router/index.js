dfdb8cb40b5a69d483e261e8853d227ab38a2399
Fernando Cueto <fernandoecueto@gmail.com>
2024-09-12 17:05:04 +0200

    protect routes on vue-router

--- ventanilla-unica-spa/src/router/index.js
+++ ventanilla-unica-spa/src/router/index.js
@@ -5,6 +5,10 @@ import LoginView from '@/views/LoginView.vue';
 import SignUpView from '@/views/SignUpView.vue';
 import PeticionView from '@/views/PeticionView.vue';
 import ListadoPeticionesView from '@/views/ListadoPeticionesView.vue';
+import axios from 'axios';
+
+const apiUrl = import.meta.env.VITE_APP_API_URL;
+const clientId = localStorage.getItem('clientId');
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -30,18 +34,34 @@ const router = createRouter({
       path: '/dashboard',
       name: 'dashboard',
       component: DashboardView,
+      meta: { requiresAuth: true },
     },
     {
       path: '/peticion',
       name: 'peticion',
       component: PeticionView,
+      meta: { requiresAuth: true },
     },
     {
       path: '/listadopeticiones',
       name: 'listadopeticiones',
       component: ListadoPeticionesView,
+      meta: { requiresAuth: true },
     },
   ],
+});
+
+router.beforeEach(async (to, from, next) => {
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    try {
+      await axios.get(apiUrl + '/api/user');
+      next();
+    } catch (error) {
+      next({ name: 'login', params: { client_id: clientId } });
+    }
+  } else {
+    next();
+  }
 });
 
 export default router;
