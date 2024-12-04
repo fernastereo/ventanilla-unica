1da08d1d14aecd3249fd2d7e9eff470bf15d1cea
Fernando Cueto <fernandoecueto@gmail.com>
2024-12-05 00:44:08 +0100

    update base web route

--- ventanilla-unica-api/routes/web.php
+++ ventanilla-unica-api/routes/web.php
@@ -3,7 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return ['Laravel' => app()->version()];
+    return [
+        'api' => 'Ventanilla Unica',
+        'Version' => app()->version(),
+        'created by' => 'No Company S.A.S.'
+    ];
 });
 
 require __DIR__.'/auth.php';
